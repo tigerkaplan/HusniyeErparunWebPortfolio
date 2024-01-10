0e239fde2c9a44e5bea0063a8b2d50c8cdaa1e63
tigerkaplan <husniyeerparun@gmail.com>
2024-01-10 14:01:27 +0000

    Component and route linked to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import { Navbar } from "../src/components/Header/Navbar";
+// Navigation
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// Pages added to the navigation
+import { About } from "../src/pages/2.About/About";
+import { Portfolio } from "./pages/3.Portfolio/Portfolio";
+import { Contact } from "../src/pages/4.Contact/Contact";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/About" element={<About />} />
+        <Route path="/Portfolio" element={<Portfolio />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }

69507e11458e7aef7bf831eeb2ff31e48174e4af
tigerkaplan <husniyeerparun@gmail.com>
2024-01-10 14:13:28 +0000

    import page navigation modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import "./App.css";
 import { Navbar } from "../src/components/Header/Navbar";
 // Navigation
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 // Pages added to the navigation
-import { About } from "../src/pages/2.About/About";
-import { Portfolio } from "./pages/3.Portfolio/Portfolio";
-import { Contact } from "../src/pages/4.Contact/Contact";
+import { Home, About, Portfolio, Contact } from "../src/pages";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
+        <Route path="/Home" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Portfolio" element={<Portfolio />} />
         <Route path="/Contact" element={<Contact />} />

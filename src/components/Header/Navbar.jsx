f9603799baea4c6618ce10193b06898f3b02ac48
tigerkaplan <husniyeerparun@gmail.com>
2024-01-10 14:34:58 +0000

    Navbar className added

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -2,13 +2,20 @@
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-  return <nav>
-  <Link to="/"> Website </Link>
-    <ul>
-      <li> <Link to="/About"> About </Link> </li>
-      <li> <Link to="/Portfolio"> Portfolio </Link> </li>
-      <li> <Link to="/Contact"> Contact </Link> </li>
-
-    </ul>
-  </nav>
-}
+  return (
+    <nav>
+      <Link to="/" className="title"> Website </Link>
+      <ul>
+        <li>
+          {" "}<Link to="/About"> About </Link>{" "}
+        </li>
+        <li>
+          {" "}<Link to="/Portfolio"> Portfolio </Link>{" "}
+        </li>
+        <li>
+          {" "}<Link to="/Contact"> Contact </Link>{" "}
+        </li>
+      </ul>
+    </nav>
+  );
+};

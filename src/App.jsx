c0fa22afb6af3d264c100d004afe6219e2aa7c7d
tigerkaplan <husniyeerparun@gmail.com>
2024-01-10 12:58:09 +0000

    Navbar tab added to function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import { Navbar } from "../src/components/Header/Navbar";
 
 function App() {
-  return;
-  <div className="App">Navbar</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;

d9363d65416506de2e7cae5af433b936a93e07f8
tigerkaplan <husniyeerparun@gmail.com>
2024-01-10 13:26:47 +0000

    Navbar/Home link added

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return <nav>
+  <Link to="/"> Website </Link>
     <ul>
       <li> <Link to="/About"> About </Link> </li>
       <li> <Link to="/Portfolio"> Portfolio </Link> </li>
       <li> <Link to="/Contact"> Contact </Link> </li>
-      <li> Contact </li>
+
     </ul>
   </nav>
 }

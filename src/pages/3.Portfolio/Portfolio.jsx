bbbb02e541f45a02816ee1c03b035d45db18e344
tigerkaplan <husniyeerparun@gmail.com>
2024-01-11 04:57:23 +0000

    Structure

--- src/pages/3.Portfolio/Portfolio.jsx
+++ src/pages/3.Portfolio/Portfolio.jsx
@@ -1,5 +1,12 @@
-export const Portfolio = () => {
+
+import React from 'react'
+
+const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div>
+      
+    </div>
   )
 }
+
+export default Portfolio

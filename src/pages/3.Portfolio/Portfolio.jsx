6ca066b171de105bbe1d40e9c37593a0dfe82051
tigerkaplan <husniyeerparun@gmail.com>
2024-01-12 04:22:30 +0000

    Portfolio structure / Portfolio.css added

--- src/pages/3.Portfolio/Portfolio.jsx
+++ src/pages/3.Portfolio/Portfolio.jsx
@@ -1,10 +1,17 @@
-import PortfolioItem from "../../components/PortfolioItem/PortfolioItem";
+import PortfolioItem from '../../components/PortfolioItem/PortfolioItem'
+
+import "./Portfolio.css" 
+
 
 export const Portfolio = () => {
   return (
     <div className='portfolio'>
       <h1>Portfolio</h1>
-      <div className="projectList">{/* display all over projects */}
+      <div className='projectList'>
+        {/* display all over projects */}
+        {portfolio.map(project => (
+          <PortfolioItem key={project.name} image={project.image} />
+        ))}
       </div>
     </div>
   )

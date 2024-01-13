a038df77ec7b93609b45c714c6848afc62e66194
tigerkaplan <husniyeerparun@gmail.com>
2024-01-13 02:38:28 +0000

    Home/Hero  section commented

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import "./App.css";
+import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar"; // Navigation
 import { Route, Routes } from "react-router-dom";
 // Pages added to the navigation
 import { Home, About, Projects, Contact } from "../src/pages";
-import { ProjectDisplay } from "./pages/ProjectDisplay/ProjectDisplay";
 
 
 function App() {
@@ -11,10 +10,9 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />          {/* //Hero section */}
         <Route path="/About" element={<About />} />
         <Route path="/Portfolio" element={<Projects />} />
-        <Route path="/project/:id" element={<ProjectDisplay />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
     </div>

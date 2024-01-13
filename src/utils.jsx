3c44762fbc3beee7fae646f5852ddc32a1a1d4b3
tigerkaplan <husniyeerparun@gmail.com>
2024-01-13 10:09:17 +0000

    assests pat updated

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,5 +1,5 @@
 //  reach to assets/images folder
 
 export const getImageUrl = (path) => {
-    return new URL (`assets/${path}`, import.meta.url).href;
+    return new URL (`/assets/${path}`, import.meta.url).href;
 };

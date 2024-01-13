cbeb9ec0174d01ba512dce5fc6b523d7fc4a13dc
tigerkaplan <husniyeerparun@gmail.com>
2024-01-13 13:38:45 +0000

    Home/hero section modified

--- src/pages/1.Home/Home.jsx
+++ src/pages/1.Home/Home.jsx
@@ -1,5 +1,25 @@
+import { getImageUrl } from "../../utils";
+import styles from "./Home.module.css";
+
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I am Husniye </h1>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. A cupiditate,
+          ipsum sunt, beatae aut voluptatibus quis iusto impedit illo velit
+          rerum fugiat eos reprehenderit? Quas modi beatae laboriosam rem
+          dolorum.
+        </p>
+        <a href="mailto:my@gmail.com" className={styles.contactBtn}>Contact Me</a>
+      </div>
+      <img src={getImageUrl("../home/hero.png")} alt="Her" className={styles.heroImg} />
+      <div className={styles.topBlur}/> 
+      <div className={styles.bottomBlur}/> 
+
+
+
+    </section>
+  );
+};

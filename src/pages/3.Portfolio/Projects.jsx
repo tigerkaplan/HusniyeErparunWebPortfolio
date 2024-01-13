ff976933037c63bded6b997f9d73d787aafbb5f9
tigerkaplan <husniyeerparun@gmail.com>
2024-01-13 03:08:49 +0000

    title updated

--- src/pages/3.Portfolio/Projects.jsx
+++ src/pages/3.Portfolio/Projects.jsx
@@ -1,17 +1,12 @@
 
-import ProjectItem from '../../components/ProjectItem/ProjectItem';
-import { projects } from "../../assets/projectData/projects.json";
 import './Projects.css';
 
 export const Projects = () => {
   return (
     <div className="projects">
-      <h1> Projects</h1>
+      <h1> Portfolio</h1>
       <div className="ProjectList">
-      {/* there is some problem here js or jsx  */}
-        {projects.map((project, idx) => (
-          <ProjectItem id={idx} name={project.name} image={project.image} />
-        ))}
+       
       </div>
     </div>
   );

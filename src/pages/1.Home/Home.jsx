d0a91ed8a547640591cb78a452f54c162bf3c84a
tigerkaplan <husniyeerparun@gmail.com>
2024-01-14 20:57:27 +0000

    React-icons installed

--- src/pages/1.Home/Home.jsx
+++ src/pages/1.Home/Home.jsx
@@ -1,25 +1,41 @@
-import { getImageUrl } from "../../utils";
-import styles from "./Home.module.css";
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import { FaGithub } from "react-icons/fa";
+import { AiOutlineLinkedin, AiOutlineMail, AiOutlineEnvironment } from "react-icons/ai";
 
 export const Home = () => {
   return (
-    <section className={styles.container}>
-      <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I am Husniye </h1>
-        <p className={styles.description}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. A cupiditate,
-          ipsum sunt, beatae aut voluptatibus quis iusto impedit illo velit
-          rerum fugiat eos reprehenderit? Quas modi beatae laboriosam rem
-          dolorum.
-        </p>
-        <a href="mailto:my@gmail.com" className={styles.contactBtn}>Contact Me</a>
+    <div id="main" className="relative h-screen">
+      <img
+        className="w-full h-full object-cover object-left scale-x-[-1]"
+        src="https://source.unsplash.com/carefree-beautiful-woman-in-knitted-sweater-and-with-windy-hair-running-on-sandy-beach-at-cold-sea-having-fun-stylish-young-happy-female-relaxing-and-enjoying-vacation-on-coast-back-view-EmvL4OW2dNI"
+        alt="Woman spreading her arms"
+      />
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+          <h1 className="sm:text-5 text-4xl font-bold text-gray-800"> 
+          I`m Husniye</h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800 ">
+            I`m a software
+            <TypeAnimation
+              sequence={["Developer", 1000, "Coder", 1000, "Graphic Designer", 1000]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: "1em", paddingLeft: "5px" }}
+              repeat={Infinity}
+              cursor={true}
+            />
+          </h2>
+          <div className="flex justify-between pt-6 max-w-[200px] w-full">
+            <FaGithub className="cursor-pointer" size={30} /> 
+            <AiOutlineLinkedin className="cursor-pointer" size={30} />
+            <AiOutlineMail className="cursor-pointer" size={30} />
+            <AiOutlineEnvironment className="cursor-pointer" size={30} />
+          </div>
+        </div>
       </div>
-      <img src={getImageUrl("../home/hero.png")} alt="Her" className={styles.heroImg} />
-      <div className={styles.topBlur}/> 
-      <div className={styles.bottomBlur}/> 
-
-
-
-    </section>
+    </div>
   );
 };
+
+export default Home;

4f6b5b2dba1857e6f100c0a0f6821766235e65f0
tigerkaplan <husniyeerparun@gmail.com>
2024-01-16 22:53:38 +0000

    Navigation route updated with footer but didn't work

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import Sidebar from "./components/Sidebar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 import Home from "../src/Pages/Home";
 import About from "../src/Pages/About";
-import Project from "../src/Pages/Project";
+import Projects from "../src/Pages/Projects";
 import Contact from "../src/Pages/Contact";
+// import Footer from "./components/Footer";
 import "./App.css";
-
-
-
 
 function App() {
   return (
-    <div>
-      <Sidebar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
+      <div>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
   );
 }
 

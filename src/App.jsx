5e62ef4aa7a3560e208aace3a385e740a0131d28
tigerkaplan <husniyeerparun@gmail.com>
2024-01-17 13:37:31 +0000

    Router modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import Sidebar from "./components/Sidebar";
 import Home from "../src/Pages/Home";
 import About from "../src/Pages/About";
 import Projects from "../src/Pages/Projects";
-import Contact from "../src/Pages/Contact";
+// import Contact from "../src/Pages/Contact";
 // import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
-      <div>
-        <Sidebar />
+    <div>
+      <Sidebar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
-      </div>
+      {/* <Footer /> */}
+    </div>
   );
 }
 

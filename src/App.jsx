3babece18bee21088e736dee412e73334ce4ba31
tigerkaplan <husniyeerparun@gmail.com>
2024-01-18 01:17:01 +0000

    App jsx updated for Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Home from "../src/Pages/Home";
 import About from "../src/Pages/About";
 import Projects from "../src/Pages/Projects";
-// import Contact from "../src/Pages/Contact";
 // import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <Sidebar />
+
+        <Sidebar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
-      {/* <Footer /> */}
+
     </div>
   );
 }

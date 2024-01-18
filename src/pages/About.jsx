785d271397f5ed6659e37a59c8b9ef8a98411781
tigerkaplan <husniyeerparun@gmail.com>
2024-01-18 01:07:31 +0000

    About page updated with bg image and footer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import { IoCloudDownloadOutline } from "react-icons/io5";
+import Footer from "../components/Footer";
 
 const PDF_FILE_URL = "http://localhost:5173/HusniyeErparun.pdf";
 
@@ -15,15 +16,37 @@ const About = () => {
   };
 
   return (
-    <div>
-      <h1>Welcome to my home page</h1>
-      <p>This is some content.</p>
+    <section>
+      <div className="flex flex-col items-center h-screen relative">
+        {/* -------------------background image ------------------------- */}
+        <img
+          className="w-full h-full object-cover object-left scale-x-[-1] absolute top-0 left-0"
+          src="/bg1.jpg"
+          alt="ebru"
+        />
+        <div className="w-full h-screen absolute top-0 left-0 bg-white/60 flex flex-col items-center justify-center">
+          <div>
+            <h1 className="sm:text-5 text-5xl font-bold text-center text-gray-800">
+              About Husniye
+            </h1>
+            <p className="text-2xl font-bold text-center">Experienced at Recruiting</p>
+            <p className="text-xl font-bold text-center">Professional Newbee Front-End Developer</p>
+            <p className="text-2xl font-bold text-center">Passionate learner</p>
+          </div>
 
-      {/* Download CV Button */}
-      <div className="flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover: scale-115 easy-in duration-200">
-        <IoCloudDownloadOutline  size={20} onClick={() => downloadFileAtURL(PDF_FILE_URL)} />
+          {/* ----------------------- Download CV Button --------------------------*/}
+          <div className="flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover: scale-10 easy-in duration-200">
+            <IoCloudDownloadOutline
+              size={20}
+              onClick={() => downloadFileAtURL(PDF_FILE_URL)}
+            />
+          </div>
+          {/* ----------------------- Download CV Button --------------------------*/}
+        </div>
+        {/* -------------------background image ------------------------- */}
       </div>
-    </div>
+      <Footer />
+    </section>
   );
 };
 
